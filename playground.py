05468fe340a8f20a7721067d2b6c97015658724f
matthew mitton <breadlicker45@users.noreply.huggingface.co>
2022-12-21 18:28:54 +0000

    Update playground.py

--- playground.py
+++ playground.py
@@ -17,7 +17,7 @@ matplotlib.use("Agg")
 sys.modules["pylab"] = pylab
 
 model_repo = "JammyMachina/elec-gmusic-familized-model-13-12__17-35-53"
-n_bar_generated = 100
+n_bar_generated = 50
 # model_repo = "JammyMachina/improved_4bars-mdl"
 # n_bar_generated = 4
 
@@ -124,11 +124,11 @@ def instrument_row(default_inst, row_id):
                 label="Instrument",
             )
             temp = gr.Dropdown(
-                [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2],
+                [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.2],
                 value=0.7,
                 label="Creativity",
             )
-            density = gr.Dropdown([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12, 13, 14], value=3, label="Note Density")
+            density = gr.Dropdown([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], value=3, label="Note Density")
 
         with gr.Column(scale=3):
             output_txt = gr.Textbox(
